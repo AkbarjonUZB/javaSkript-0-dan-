6cab87cb0ab6ded0f084dda119f636156244e7bf
Akbarjon <akbarjonraxmonov@1997gmail.com>
2024-01-02 17:23:55 +0500

    yangi dars

---
+++ js.js
@@ -0,0 +1,17 @@
+//  ja 0-dan boshlab 
+// function pow(x, n) {
+//    let result = 1;
+ 
+//    for (let i = 0; i < n; i++) {
+//      result *= x;
+//    }
+ 
+//    return result;
+//  }
+
+let user = {     // an object
+   name: "John",  // by key "name" store value "John"
+   age: 30        // by key "age" store value 30
+};
+console.log(user)
+
